09b59446f46d582a99222ee4426890b3c08c5257
Stephan Fuchs <stephan@laborfrust.de>
2020-03-20 17:32:07 +0100

    changed: top menu

--- src/app/core/components/navigation-bar/navigation-bar.component.ts
+++ src/app/core/components/navigation-bar/navigation-bar.component.ts
@@ -26,29 +26,9 @@ export class NavigationBarComponent implements OnInit {
     return false;
   }
 
-  toggleHomepage() {
-      this.router.navigateByUrl('/home');
+  goto(route: string) {
+      this.router.navigateByUrl(route);
     }
-
-  toggleMPA() {
-    this.router.navigateByUrl('/mpa');
-  }
-
-  toggleProphane() {
-    this.router.navigateByUrl('/prophane');
-  }
-
-  toggleTOS() {
-    this.router.navigateByUrl('/termsofservice');
-  }
-
-  togglePP() {
-    this.router.navigateByUrl('/privacypolicy');
-  }
-
-  toggleImpressum() {
-    this.router.navigateByUrl('/impressum');
-  }
 
   ngOnInit(): void {
     this.authGuard.user.subscribe(res => {
