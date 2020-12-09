17a48d5d5bc77e4e5573f3b09676d40f0129d57f
Kay Schallert <kay.schallert@ovgu.de>
2020-12-09 14:44:12 +0100

    Add global-http-interceptor

---
+++ src/app/core/services/global-http-interceptor.service.ts
@@ -0,0 +1,49 @@
+import {Injectable} from '@angular/core';
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse, HttpErrorResponse} from '@angular/common/http';
+import {Observable, of, throwError} from 'rxjs';
+import {catchError, map} from 'rxjs/operators';
+import {Router} from '@angular/router';
+import {AuthGuard} from './auth-guard.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+@Injectable()
+export class GlobalHttpInterceptorService implements HttpInterceptor {
+
+  constructor(public router: Router, private authGuard: AuthGuard) {
+  }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(req).pipe(
+      catchError((error) => {
+        if (error instanceof HttpErrorResponse) {
+          if (error.error instanceof ErrorEvent) {
+            console.error('Error Event');
+          } else {
+            console.log(`error status : ${error.status} ${error.statusText}`);
+            switch (error.status) {
+              case 401:      // login
+                this.authGuard.logout();
+                this.router.navigateByUrl('/login');
+                break;
+              case 403:     // forbidden
+                this.authGuard.logout();
+                this.router.navigateByUrl('/login');
+                break;
+              default:
+                // handle other errors
+                break;
+            }
+          }
+          return throwError(error);
+        } else {
+          console.error('some thing else happened');
+          return next.handle(req);
+        }
+      })
+    );
+  }
+}
+
