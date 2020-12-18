079204d0caac28c7b5cbfaf0cebd3c634f1c7e8e
Henning Schiebenhoefer <henning.schiebenhoefer@posteo.de>
2020-12-18 20:53:48 +0100

    result-view-component: show single-row table for requested result

--- src/app/prophane/components/prophane-result-view/prophane-result-view.component.ts
+++ src/app/prophane/components/prophane-result-view/prophane-result-view.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { JobService } from '../../job.service';
+import {ProphaneJobObject} from '../../objects/prophanejobjson';
 
 @Component({
   selector: 'app-prophane-result-view',
@@ -6,10 +10,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./prophane-result-view.component.css']
 })
 export class ProphaneResultViewComponent implements OnInit {
+  job: ProphaneJobObject;
+  isUpdating = true;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private jobService: JobService
+  ) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.getJob();
+    this.isUpdating = false;
+  }
+
+  getJob(): void {
+    const uuid = this.route.snapshot.paramMap.get('job_uuid');
+    this.jobService.getJobs()
+      .subscribe(jobs => this.job = jobs.filter(j => j.prophaneJobUUID === uuid)[0]);
   }
 
 }
