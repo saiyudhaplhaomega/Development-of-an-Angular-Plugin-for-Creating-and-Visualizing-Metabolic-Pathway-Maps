f5b2e3201ed54bf30db225dffe4c9926cbbdea93
gHatlar <ghatlar@gmail.com>
2023-08-14 10:10:11 +0200

    initialize route

--- projects/signaling-tool/src/app/app-routing.module.ts
+++ projects/signaling-tool/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Component1Component } from './component1/component1.component';
+import { Component2Component } from './component2/component2.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: Component1Component}, 
+  { path: 'blog', component: Component2Component}, 
+
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'home' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+ 
+
