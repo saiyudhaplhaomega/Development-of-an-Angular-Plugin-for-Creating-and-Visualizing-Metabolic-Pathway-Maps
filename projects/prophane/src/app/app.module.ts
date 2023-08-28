14422a19f2869eb01862ed4f33270bcc074c15b1
Juliane Schmachtenberg <jule-schmachtenberg@web.de>
2023-08-28 15:17:19 +0200

    moved footer to shared_lib

--- projects/prophane/src/app/app.module.ts
+++ projects/prophane/src/app/app.module.ts
@@ -18,6 +18,9 @@ import { PrivacyPolicyPageComponent } from './components/privacy-policy-page/pri
 import { TermsOfServicePageComponent } from './components/terms-of-service-page/terms-of-service-page.component';
 import { SafePipe } from './safe.pipe';
 
+import { FooterComponent } from 'projects/shared-lib/src/lib/components/footer/page-footer';
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -40,4 +43,7 @@ import { SafePipe } from './safe.pipe';
   ],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
+
+
+
---
+++ projects/shared-lib/src/lib/components/footer/page-footer.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
+import { AuthGuard, AuthService } from 'dist/shared-lib';
+import { UserToken } from 'dist/shared-lib';
+
+@Component({
+  selector: 'app-page-footer',
+  templateUrl: './page-footer.html',
+  styleUrls: ['./page-footer.scss'],
+})
+export class FooterComponent implements OnInit {
+  user: UserToken;
+  guest: boolean;
+
+  constructor(public authService: AuthService) {}
+
+  ngOnInit(): void {
+    // this.authService.user.subscribe((res) => {
+    //   this.user = res;
+    // });
+    // this.authService.guest.subscribe((res) => {
+    //   this.guest = res;
+    // });
+  }
+}
