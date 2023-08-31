02be2e786d3e6e2335fdd141ba65bb331de8fb26
kay.schallert <kay.schallert@isas.de>
2023-08-31 13:58:35 +0200

    Material Icons for MPA (index.hml), OFF refactor

--- projects/ofs/src/app/app.component.ts
+++ projects/ofs/src/app/app.component.ts
@@ -1,26 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { filter } from 'rxjs';
-import { NavigationRoute } from 'shared-lib';
+import { AuthService, HttpClientService, NavigationRoute } from 'shared-lib';
 import { RouteStateService } from './services/route-state.service';
-import { OfsHttpClientService } from './services/ofs-http-client.service';
 
 @Component({
-  selector: 'ofs-root',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   constructor(
-    private http: OfsHttpClientService,
+    private http: HttpClientService,
     private router: Router,
-    private routeState: RouteStateService
+    private routeState: RouteStateService,
+    private authService: AuthService
   ) {}
 
+  title = 'Omics Feature Finder';
   routes: NavigationRoute[] = [
-    { route: '/home', label: 'home' },
-    { route: '/workflow', label: 'workflow' },
+    { route: '/workflow', label: 'workflow', requireAuth: false },
   ];
+  homelink: NavigationRoute = {
+    route: '/home',
+    label: 'Home',
+    requireAuth: false,
+  };
 
   ngOnInit() {
     this.router.events
@@ -28,5 +33,6 @@ export class AppComponent implements OnInit {
       .subscribe((event: NavigationEnd) => {
         this.routeState.setCurrentRoute(event.url);
       });
+    this.authService.initializeOAuth();
   }
 }
--- projects/ofs/src/app/app.module.ts
+++ projects/ofs/src/app/app.module.ts
@@ -1,28 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavToolbarModule } from 'shared-lib';
-import { LoginModule } from 'shared-lib';
 
-import { MatButtonModule } from '@angular/material/button';
 import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
 import { OfsLandingPageComponent } from './ofs-landing-page/ofs-landing-page.component';
+import { StandardPageLayoutModule } from 'shared-lib';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    OfsLandingPageComponent],
+  declarations: [AppComponent, OfsLandingPageComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    NavToolbarModule,
-    MatButtonModule,
     HttpClientModule,
-    LoginModule,
+    MatButtonModule,
+    StandardPageLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
