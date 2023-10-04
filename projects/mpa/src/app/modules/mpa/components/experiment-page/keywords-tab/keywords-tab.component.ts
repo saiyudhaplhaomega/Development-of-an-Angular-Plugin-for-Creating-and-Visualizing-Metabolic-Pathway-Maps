704cf7b43b7b6f87513b247b06f1c6b9b03fb9cd
LuKnoche <ludwig98knoche@gmail.com>
2023-10-04 10:51:02 +0200

    mpa: keywords-tab implemented preliminary layout and functions

--- projects/mpa/src/app/modules/mpa/components/experiment-page/keywords-tab/keywords-tab.component.ts
+++ projects/mpa/src/app/modules/mpa/components/experiment-page/keywords-tab/keywords-tab.component.ts
@@ -1,6 +1,5 @@
-import { ArrayDataSource } from '@angular/cdk/collections';
-import { FlatTreeControl, NestedTreeControl } from '@angular/cdk/tree';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatAccordion } from '@angular/material/expansion';
 import { KeywordJSONObject, UniProtKeyword, UniProtKeywordCategories } from '../../../model/keywordjson';
 
 @Component({
@@ -10,25 +9,70 @@ import { KeywordJSONObject, UniProtKeyword, UniProtKeywordCategories } from '../
 })
 export class KeywordsTabComponent implements OnInit {
 
+  //TODO implement buttons to open/close all expansion-panels with accordion
+  @ViewChild(MatAccordion) accordion: MatAccordion;
+
   //TODO delete mock-data
   keywordsJSON: KeywordJSONObject = new KeywordJSONObject();
-
-  //TODO fill tree with hierarchical data based on keywordsJSON
-  dataSource: ArrayDataSource<UniProtKeyword>;
-  //treeControl: FlatTreeControl<UniProtKeyword>;
+  dataMap: Map<UniProtKeywordCategories, UniProtKeyword[]>;
+  keyArray: UniProtKeywordCategories[];
+  keywordCategories = UniProtKeywordCategories;
+  filterString: string;
 
   constructor() {
     this.keywordsJSON.keywords = [
-      {category: UniProtKeywordCategories.BIOLOGICAL_PROCESS, description: 'desc1'},
-      {category: UniProtKeywordCategories.BIOLOGICAL_PROCESS, description: 'desc2'},
-      {category: UniProtKeywordCategories.BIOLOGICAL_PROCESS, description: 'desc3'},                       
+      { category: UniProtKeywordCategories.BIOLOGICAL_PROCESS, description: 'desc1' },
+      { category: UniProtKeywordCategories.LIGAND, description: 'asdf' },
+      { category: UniProtKeywordCategories.DISEASE, description: 'desc3' },
+      { category: UniProtKeywordCategories.DISEASE, description: 'efef' },
+      { category: UniProtKeywordCategories.BIOLOGICAL_PROCESS, description: 'too' },
+      { category: UniProtKeywordCategories.LIGAND, description: 'nnnv' }
+
     ]
-
-    this.dataSource = new ArrayDataSource(this.keywordsJSON.keywords)
-    //TODO instanciate treeControl
-   }
-
-  ngOnInit(): void {
+    this.dataMap = new Map<UniProtKeywordCategories, UniProtKeyword[]>();
+    this.keyArray = [];
   }
 
+  ngOnInit(): void {
+    // this.keywordsJSON.keywords.sort((a,b) => a.category.localeCompare(b.category));
+    this.setData(this.keywordsJSON.keywords)
+  }
+
+  setData(keywords: UniProtKeyword[]): void {
+    for (let i in keywords) {
+      let categoryArray = this.dataMap.get(keywords[i].category);
+      if (categoryArray) {
+        categoryArray.push(keywords[i]);
+      } else {
+        categoryArray = [keywords[i]];
+      }
+      categoryArray = this.sortData('category',categoryArray)
+      this.dataMap.set(keywords[i].category,categoryArray);
+    }
+
+    this.dataMap.forEach((value,key) => {
+      this.keyArray.push(key);
+    })
+  }
+
+  //TODO implement insertion into detail-component and display of that component
+  keywordClicked(keyword: UniProtKeyword): void {
+    console.log(keyword);
+  }
+
+  sortData(sortBy: string, data: UniProtKeyword[]): UniProtKeyword[] {
+    if (sortBy == 'category') {
+      data.sort((a,b) => a.category.localeCompare(b.category));
+      return data;
+    } else if (sortBy == 'quant') {
+      //TODO
+      console.log('sortBy quant not implemented yet!')
+      return []
+    }
+  }
+
+  //TODO implement
+  applyFilter(): void {
+   
+  }
 }
