31e357cc3eb2a748054ce74b6fdf5336de26c90e
Bene <beneschwab92@gmail.com>
2023-11-09 08:28:34 +0100

    Remove httpclient, but got 2 modules

--- projects/metadata-tool/src/ownStories/SDRFTable.stories.ts
+++ projects/metadata-tool/src/ownStories/SDRFTable.stories.ts
@@ -1,34 +1,34 @@
-// SDRFTableComponent.stories.ts
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MetadataWorkflowComponent } from '../app/metadata-workflow/metadata-workflow.component';
-import { moduleMetadata } from '@storybook/angular';
+import { applicationConfig } from '@storybook/angular';
 import { HttpClientModule } from '@angular/common/http';
-
-
+import { HttpClientService } from 'shared-lib';
+import { importProvidersFrom } from '@angular/core';
 
 export default {
   title: 'Project/SDRF Table Component',
   component: MetadataWorkflowComponent,
   decorators: [
-    moduleMetadata({
-      declarations: [MetadataWorkflowComponent],
-      imports: [
-        HttpClientModule, // Add this line
-        BrowserAnimationsModule,
-        MatButtonModule,
-        MatInputModule,
-        FormsModule,
-        ReactiveFormsModule,
-        // any other modules that your component requires
+    applicationConfig({
+      providers: [
+        importProvidersFrom(BrowserAnimationsModule),
+        importProvidersFrom(HttpClientModule),
+        // Any other modules that your component requires
       ],
     }),
   ],
 };
+
 const Template = (args) => ({
   props: args,
+  applicationConfig: {
+    providers: [
+      // Provide HttpClientService, which will use HttpClient from HttpClientModule
+      HttpClientService,
+      // Any other services that need to be provided
+    ],
+  },
   // ... other template setup
 });
 
