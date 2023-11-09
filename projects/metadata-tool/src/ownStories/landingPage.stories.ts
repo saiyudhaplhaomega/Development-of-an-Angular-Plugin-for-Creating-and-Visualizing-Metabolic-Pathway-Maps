2dc313ebdfb773463d47757b9ddba2f8279e9979
Bene <beneschwab92@gmail.com>
2023-11-09 11:52:28 +0100

    refactor

--- projects/metadata-tool/src/ownStories/landingPage.stories.ts
+++ projects/metadata-tool/src/ownStories/landingPage.stories.ts
@@ -3,6 +3,16 @@ import {  MetadataLandingPageComponent  } from '../app/metadata-landing-page/met
 import { RouterModule } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterTestingModule } from '@angular/router/testing'; // Make sure to import this
+import { StandardPageLayoutModule } from 'shared-lib';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from '../app/app-routing.module';
+import { WorkflowModule } from '../app/metadata-workflow/metadata-workflow.module';
+import { MetaDataCheckboxSelectionModule } from '../app/metadata-checkboxselection/metadata-checkboxselection.module';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { MetadataDownloadpageModule } from '../app/metadata-downloadpage/metadata-downloadpage.module';
+import { MetadataUploadpageModule } from '../app/metadata-uploadpage/metadata-uploadpage.module';
+
 
 export default {
   title: 'Project/MetadataLandingPageComponent',
@@ -11,8 +21,14 @@ export default {
     moduleMetadata({
       declarations: [MetadataLandingPageComponent],
       imports: [
-        MatButtonModule, // Import Angular Material Button Module
+        // Top level
+        BrowserModule,
+        BrowserAnimationsModule,
+
+        // specific
         RouterTestingModule, // Mock routing
+        HttpClientModule,
+
         // Import other Material modules as needed
       ],
     }),
