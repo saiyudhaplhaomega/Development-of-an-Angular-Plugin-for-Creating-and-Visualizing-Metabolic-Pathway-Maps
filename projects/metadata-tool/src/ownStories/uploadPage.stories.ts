4b65d369d0813bef910c165a89a736a643dd2f20
Bene <beneschwab92@gmail.com>
2023-11-09 09:39:12 +0100

    fixed uploadPage

--- projects/metadata-tool/src/ownStories/uploadPage.stories.ts
+++ projects/metadata-tool/src/ownStories/uploadPage.stories.ts
@@ -1,30 +1,32 @@
-// Import the necessary decorators and modules from Storybook and Angular
-import { Meta, moduleMetadata } from '@storybook/angular';
+import { applicationConfig, Meta, StoryObj } from '@storybook/angular';
 import { MatButtonModule } from '@angular/material/button';
 import { MetadataUploadpageComponent } from '../app/metadata-uploadpage/metadata-uploadpage.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
+import { OAuthModule } from 'angular-oauth2-oidc';
+import { MatDialogModule } from '@angular/material/dialog';
 
 export default {
   title: 'Project/File Upload Component',
   component: MetadataUploadpageComponent,
   decorators: [
-    moduleMetadata({
-      // If your component uses additional modules, declare them here
-      imports: [MatButtonModule],
-      // If your component uses services or other components, declare them here
-      declarations: [MetadataUploadpageComponent],
-      // Add any necessary providers here
-      // providers: [...],
+    applicationConfig({
+      providers: [
+        importProvidersFrom(BrowserAnimationsModule),
+        importProvidersFrom(HttpClientModule),
+        importProvidersFrom(MatButtonModule),
+        importProvidersFrom(OAuthModule.forRoot()),
+        importProvidersFrom(MatDialogModule),
+      ],
     }),
   ],
 } as Meta;
 
-// Template function to define how the component should be rendered
-const Template= (args) => ({
-  props: args,
-});
+type Story = StoryObj<typeof MetadataUploadpageComponent>;
 
-// Default story using the Template function
-export const Default = Template.bind({});
-Default.args = {
-  // If your component takes inputs, provide default values here
+export const Default: Story = {
+  render: () => ({
+    props: {},
+  }),
 };
