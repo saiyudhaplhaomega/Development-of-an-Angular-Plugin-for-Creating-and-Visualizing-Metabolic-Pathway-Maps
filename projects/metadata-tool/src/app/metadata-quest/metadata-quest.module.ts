53e069e4bc14527a57427d20820fb9e2e28fc63d
Bene <beneschwab92@gmail.com>
2023-11-10 17:14:44 +0100

    refactor

--- projects/metadata-tool/src/app/metadata-quest/metadata-quest.module.ts
+++ projects/metadata-tool/src/app/metadata-quest/metadata-quest.module.ts
@@ -1,13 +1,26 @@
-// questionnaire.module.ts
+// based of this concept: https://dev.to/sp90/angular-reactive-forms-conditional-validation-4aop
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MetadataQuestComponent } from './metadata-quest.component';
 import { MetadataQuestRoutingModule } from './metadata-quest-routing.module';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [MetadataQuestComponent],
-  imports: [CommonModule, ReactiveFormsModule, MetadataQuestRoutingModule],
+  imports: [
+    // base imports
+    CommonModule,
+    ReactiveFormsModule,
+
+    //material design imports
+    MetadataQuestRoutingModule,
+    MatRadioModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
   exports: [
     MetadataQuestComponent, // Export if it will be used outside the module
   ],
