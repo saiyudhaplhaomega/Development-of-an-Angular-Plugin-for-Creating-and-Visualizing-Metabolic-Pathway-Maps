3ef627de7f9e741784b19e197c3c04d4225baab9
Bene <beneschwab92@gmail.com>
2023-11-22 18:38:19 +0100

    aider: Updated the `Default.args` with mocked data for the `MetadataQuestFormComponent` in the Storybook story.
    # Aider chat conversation:
    
    ASSISTANT: To mock the data for the `MetadataQuestFormComponent` in the Storybook story, we need to provide default arguments that represent the data the component expects. We will update the `metadata-quest.component.stories.ts` file to include these mocked data.
    
    Here is the *SEARCH/REPLACE block* for the story file:
    
    projects/metadata-tool/src/app/components/metadata-quest-form/metadata-quest.component.stories.ts
    ```typescript
    <<<<<<< SEARCH
    export const Default = Template.bind({});
    Default.args = {
      // Provide default args for your component here
    };
    =======
    export const Default = Template.bind({});
    Default.args = {
      model: {
        experimentType: 'metaproteomics',
        metagenomes: 'ecological',
        ecologicalMetagenomes: 'gut',
        technologyType: 'shotgun',
        instrument: 'Orbitrap',
        cleavantAgentDetails: 'inGel',
        modificationParameters: 'Trypsin',
        assayName: 'Assay X',
        label: 'Label Y',
        group: 'Group Z',
        project: 'Project Alpha',
        program: 'Program Beta',
        factorValue: 'Temperature'
      },
      form: new FormGroup({}),
      fields: [
        // The fields array should be structured as it is expected by the component.
        // This is a simplified example based on the provided component code.
        {
          type: 'stepper',
          fieldGroup: [
            {
              props: { label: 'Experiment Settings' },
              fieldGroup: [
                // Add the rest of the field configuration here
              ],
            },
            // Add other steps configuration here
          ],
        },
      ],
      options: {},
      specificMetagenomeOptions: [
        { value: 'gut', label: 'Gut' },
        { value: 'human', label: 'Human' },
        // ... other options
      ],
      metagenomeTypeOptions: [
        { value: 'ecological', label: 'Ecological' },
        { value: 'organism', label: 'Organism' },
        { value: 'synthetic', label: 'Synthetic' },
        { value: 'metagenome', label: 'Metagenome' },
      ],
      technologyTypeOptions: [
        { value: 'shotgun', label: 'Shotgun' },
        // Add other technology types as needed
      ],
      digestionMethodOptions: [
        { value: 'inGel', label: 'In Gel' },
        { value: 'FASP', label: 'FASP' },
        // Add other digestion methods as needed
      ],
    };
    >>>>>>> REPLACE
    ```
    
    This block updates the `Default.args` with mocked data that corresponds to the structure of the `model`, `form`, `fields`, `options`, and other properties used by the `MetadataQuestFormComponent`. You will need to fill in the `fieldGroup` array with the actual field configurations as expected by the component. The provided example includes the structure and some mock data based on the component's TypeScript file.

--- projects/metadata-tool/src/app/components/metadata-quest-form/metadata-quest.component.stories.ts
+++ projects/metadata-tool/src/app/components/metadata-quest-form/metadata-quest.component.stories.ts
@@ -18,7 +18,59 @@ const Template = (args: MetadataQuestFormComponent) => ({
 
 export const Default = Template.bind({});
 Default.args = {
-  // Provide default args for your component here
+  model: {
+    experimentType: 'metaproteomics',
+    metagenomes: 'ecological',
+    ecologicalMetagenomes: 'gut',
+    technologyType: 'shotgun',
+    instrument: 'Orbitrap',
+    cleavantAgentDetails: 'inGel',
+    modificationParameters: 'Trypsin',
+    assayName: 'Assay X',
+    label: 'Label Y',
+    group: 'Group Z',
+    project: 'Project Alpha',
+    program: 'Program Beta',
+    factorValue: 'Temperature'
+  },
+  form: new FormGroup({}),
+  fields: [
+    // The fields array should be structured as it is expected by the component.
+    // This is a simplified example based on the provided component code.
+    {
+      type: 'stepper',
+      fieldGroup: [
+        {
+          props: { label: 'Experiment Settings' },
+          fieldGroup: [
+            // Add the rest of the field configuration here
+          ],
+        },
+        // Add other steps configuration here
+      ],
+    },
+  ],
+  options: {},
+  specificMetagenomeOptions: [
+    { value: 'gut', label: 'Gut' },
+    { value: 'human', label: 'Human' },
+    // ... other options
+  ],
+  metagenomeTypeOptions: [
+    { value: 'ecological', label: 'Ecological' },
+    { value: 'organism', label: 'Organism' },
+    { value: 'synthetic', label: 'Synthetic' },
+    { value: 'metagenome', label: 'Metagenome' },
+  ],
+  technologyTypeOptions: [
+    { value: 'shotgun', label: 'Shotgun' },
+    // Add other technology types as needed
+  ],
+  digestionMethodOptions: [
+    { value: 'inGel', label: 'In Gel' },
+    { value: 'FASP', label: 'FASP' },
+    // Add other digestion methods as needed
+  ],
 };
 
 // You can add more stories to represent different states of your component
