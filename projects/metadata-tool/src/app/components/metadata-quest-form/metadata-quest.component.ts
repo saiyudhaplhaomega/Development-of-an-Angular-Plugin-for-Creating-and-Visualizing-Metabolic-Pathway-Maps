7ad0d888c7daa3ed2b94665a7fefd06da1223c91
Bene <beneschwab92@gmail.com>
2023-11-22 18:39:58 +0100

    aider: Imported `FormGroup` from `@angular/forms` in `metadata-quest.component.ts`.
    # Aider chat conversation:
    
    USER: FormGroup is not defined
    
    ASSISTANT: It appears that the `FormGroup` is not defined in the context where it's being used. This typically happens when the `FormGroup` class is not imported from the `@angular/forms` package in the TypeScript file where it's being utilized.
    
    To resolve this issue, we need to ensure that `FormGroup` is imported in the `metadata-quest.component.ts` file. Since you've provided the `metadata-quest.component.ts` file as a *read-write* file, I will proceed with the necessary changes.
    
    Here is the *SEARCH/REPLACE block* for the necessary change:
    
    projects/metadata-tool/src/app/components/metadata-quest-form/metadata-quest.component.ts
    ```typescript
    <<<<<<< SEARCH
    import { Component } from '@angular/core';
    =======
    import { Component } from '@angular/core';
    import { FormGroup } from '@angular/forms';
    >>>>>>> REPLACE
    ```
    
    This change will import `FormGroup` from `@angular/forms`, which should resolve the error you're encountering. If the error persists, please ensure that the `FormGroup` is being used correctly within the component.

--- projects/metadata-tool/src/app/components/metadata-quest-form/metadata-quest.component.ts
+++ projects/metadata-tool/src/app/components/metadata-quest-form/metadata-quest.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
 import { FormlyFieldConfig, FormlyFormOptions } from '@ngx-formly/core';
 
 
