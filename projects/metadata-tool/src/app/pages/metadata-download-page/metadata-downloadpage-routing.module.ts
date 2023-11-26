690c2dc8331dcc268a56a74e78ec1c144b843b04
Bene <beneschwab92@gmail.com>
2023-11-26 14:33:05 +0100

    added prettier and eslint config

--- projects/metadata-tool/src/app/pages/metadata-download-page/metadata-downloadpage-routing.module.ts
+++ projects/metadata-tool/src/app/pages/metadata-download-page/metadata-downloadpage-routing.module.ts
@@ -1,13 +1,11 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { MetadataDownloadpageComponent } from './metadata-downloadpage.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { MetadataDownloadpageComponent } from "./metadata-downloadpage.component";
 
-const routes: Routes = [
-  {path: '', component: MetadataDownloadpageComponent}
-];
+const routes: Routes = [{ path: "", component: MetadataDownloadpageComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class MetadataDownloadpageRoutingModule { }
+export class MetadataDownloadpageRoutingModule {}
