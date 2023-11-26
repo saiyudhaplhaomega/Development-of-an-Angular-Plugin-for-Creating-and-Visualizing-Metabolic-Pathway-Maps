196f02084f582ec46b1bc727ae7937e7ca634999
Bene <beneschwab92@gmail.com>
2023-11-26 14:47:45 +0100

    added html to prettier

--- projects/metadata-tool/src/app/components/upload-container/metadata-uploadpage.component.ts
+++ projects/metadata-tool/src/app/components/upload-container/metadata-uploadpage.component.ts
@@ -22,7 +22,6 @@ export class MetadataUploadContainerComponent implements OnInit {
     progress: number;
   }>();
 
-
   model: FileMetadata;
 
 <<<<<<< HEAD
@@ -133,48 +132,52 @@ export class MetadataUploadContainerComponent implements OnInit {
 =======
   pipelines: Pipeline[] = [
     {
-      value: 'generic_mzid_mzml',
-      viewValue: 'Generic (mzid+mzml)',
-      acceptedDataTypes: '.mzid , .mzml',
-      acceptedDataTypesRegex: '\\.mzid|\\.mzml',
-      matchingFiles: {},
+      value: "generic_mzid_mzml",
+      viewValue: "Generic (mzid+mzml)",
+      acceptedDataTypes: ".mzid , .mzml",
+      acceptedDataTypesRegex: "\\.mzid|\\.mzml",
+      matchingFiles: {}
     },
     {
-      value: 'metaproteomeanalyzer',
-      viewValue: 'MetaProteomeAnalyzer',
-      acceptedDataTypes: '.mgf , .csv',
-      acceptedDataTypesRegex: '\\.mgf|\\.csv',
+      value: "metaproteomeanalyzer",
+      viewValue: "MetaProteomeAnalyzer",
+      acceptedDataTypes: ".mgf , .csv",
+      acceptedDataTypesRegex: "\\.mgf|\\.csv",
       matchingFiles: {
-        spectra: '*_MixA.mgf',
-        Peptide: 'Peptides_*_MixA.mgf',
-        PSM: 'PSMs_*_MixA.csv',
-      },
+        spectra: "*_MixA.mgf",
+        Peptide: "Peptides_*_MixA.mgf",
+        PSM: "PSMs_*_MixA.csv"
+      }
     },
     {
-      value: 'proteomediscoverer',
-      viewValue: 'ProteomeDiscoverer',
-      acceptedDataTypes: '.mgf , .csv',
-      acceptedDataTypesRegex: '\\.mgf|\\.csv',
+      value: "proteomediscoverer",
+      viewValue: "ProteomeDiscoverer",
+      acceptedDataTypes: ".mgf , .csv",
+      acceptedDataTypesRegex: "\\.mgf|\\.csv",
       matchingFiles: {
-        spectra: 'Fisdljsd.mgf',
-        Peptide: 'Fisdljsd.mgf',
-        PSM: 'PSMs_Fisdljsd.csv',
-      },
+        spectra: "Fisdljsd.mgf",
+        Peptide: "Fisdljsd.mgf",
+        PSM: "PSMs_Fisdljsd.csv"
+      }
     },
     {
-      value: 'generic_mgf_mzid',
-      viewValue: 'Generic (mgf+mzid)',
-      acceptedDataTypes: '.mgf , .mzid',
-      acceptedDataTypesRegex: '\\.mgf|\\.mzid',
-      matchingFiles: {},
-    },
+      value: "generic_mgf_mzid",
+      viewValue: "Generic (mgf+mzid)",
+      acceptedDataTypes: ".mgf , .mzid",
+      acceptedDataTypesRegex: "\\.mgf|\\.mzid",
+      matchingFiles: {}
+    }
   ];
 >>>>>>> 55380e6 (bugfix)
 
   selectedPipeline: Pipeline = this.pipelines[1];
   uploadDialogId: string;
+<<<<<<< HEAD
   uploadProgress: number = 0;
 >>>>>>> 41f5c08 (refactor)
+=======
+  uploadProgress = 0;
+>>>>>>> a372259 (added html to prettier)
   showContainer = true;
   metadataUploadJson: MetaDataUploadJson;
 
@@ -206,6 +209,7 @@ export class MetadataUploadContainerComponent implements OnInit {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
     console.log("Selected Files:", this.selectedFiles); // Debugging
 
@@ -219,6 +223,9 @@ export class MetadataUploadContainerComponent implements OnInit {
 >>>>>>> 41f5c08 (refactor)
 =======
     console.log('Selected Files:', this.selectedFiles); // Debugging
+=======
+    console.log("Selected Files:", this.selectedFiles); // Debugging
+>>>>>>> a372259 (added html to prettier)
 
 >>>>>>> 41f5c08 (refactor)
     // Hide certain UI elements during upload
@@ -239,6 +246,7 @@ export class MetadataUploadContainerComponent implements OnInit {
     // Log the model for debugging
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     console.log('Model from pipeline:', JSON.stringify(this.model));
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -253,6 +261,9 @@ export class MetadataUploadContainerComponent implements OnInit {
 >>>>>>> a372259 (added html to prettier)
 =======
 >>>>>>> 41f5c08 (refactor)
+=======
+    console.log("Model from pipeline:", JSON.stringify(this.model));
+>>>>>>> a372259 (added html to prettier)
     // Update the metadata with the selected pipeline information
     this.dataService.updateAllMetaDataUploadJson(this.model);
 
