58d4c41908e9e3653e962cf7a353bce7422b27f1
Benedikt <beneschwab92@gmail.com>
2023-11-29 12:43:26 +0100

    add mat button to landing page module

--- projects/metadata-tool/src/app/app.module.ts
+++ projects/metadata-tool/src/app/app.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { MetadataLandingPageComponent } from "./pages/metadata-landing-page/metadata-landing-page.component";
-import { MatButtonModule } from "@angular/material/button";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { HttpClientModule } from "@angular/common/http";
 import { StandardPageLayoutModule } from "dist/shared-lib";
@@ -11,6 +9,7 @@ import { FormlyModule } from "@ngx-formly/core";
 import { ReactiveFormsModule } from "@angular/forms";
 import { FormlyMaterialModule } from "@ngx-formly/material";
 import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
 
 @NgModule({
   declarations: [AppComponent],
--- projects/metadata-tool/src/app/pages/metadata-landing-page/landing-page.module.ts
+++ projects/metadata-tool/src/app/pages/metadata-landing-page/landing-page.module.ts
@@ -1,12 +1,14 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 import { MetadataLandingPageComponent } from "./metadata-landing-page.component";
 import { NgParticlesModule } from "ng-particles";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
 
 @NgModule({
   declarations: [
     MetadataLandingPageComponent // Declare the component here
   ],
-  imports: [NgParticlesModule],
+  imports: [NgParticlesModule, MatIconModule, MatButtonModule],
   exports: [
     MetadataLandingPageComponent // Now you can export it
   ]
