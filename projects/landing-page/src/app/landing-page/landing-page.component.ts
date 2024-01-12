6483bb19de8f280fab3efb41bc8f1028bb64769a
Daniel Kautzner <kautzner97@gmail.com>
2024-01-12 08:30:38 +0100

    styling landing page

--- projects/landing-page/src/app/landing-page/landing-page.component.ts
+++ projects/landing-page/src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 import { ToolCardComponent } from './tool-card/tool-card.component';
 @Component({
   selector: 'landing-page',
