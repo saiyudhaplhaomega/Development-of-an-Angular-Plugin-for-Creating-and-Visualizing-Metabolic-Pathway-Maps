197fb018e18de54022be9ead68565e8e34a57555
Juliane Schmachtenberg <jule-schmachtenberg@web.de>
2024-02-16 12:03:18 +0100

    removed color styling

--- projects/prophane/src/app/components/privacy-policy-page/privacy-policy-page.component.ts
+++ projects/prophane/src/app/components/privacy-policy-page/privacy-policy-page.component.ts
@@ -3,7 +3,7 @@ import { Component} from '@angular/core';
 @Component({
   selector: 'app-privacy-policy-page',
   templateUrl: './privacy-policy-page.component.html',
-  styleUrls: ['./privacy-policy-page.component.css']
+  styleUrls: ['./privacy-policy-page.component.scss']
 })
 export class PrivacyPolicyPageComponent {
 
