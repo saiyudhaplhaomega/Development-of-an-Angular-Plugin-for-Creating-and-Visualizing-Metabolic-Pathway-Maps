dcaff9a7e5b7f4188785c41f0f8315daf455e3c4
Juliane Schmachtenberg <jule-schmachtenberg@web.de>
2024-02-16 09:26:27 +0100

    t2p changes

--- projects/tax2proteome/src/app/app.component.ts
+++ projects/tax2proteome/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'tax2proteome';
